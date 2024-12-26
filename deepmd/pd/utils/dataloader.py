JunnHuo/deepmd-kit

commit 429fbb87ea39d921b381555a5c51b1d8c354b275

fix: fix seed with multiple ranks (#4479)
Fix #4440.

<!-- This is an auto-generated comment: release notes by coderabbit.ai
-->
## Summary by CodeRabbit

- **New Features**
- Enhanced seed handling to support both single integers and lists for
improved randomness in distributed training.
	- Added logging for neighbor statistics calculation during training.

- **Bug Fixes**
- Improved error handling in data loading processes to ensure
robustness.

- **Documentation**
- Updated documentation for methods related to seed and batch size
management for clarity.
<!-- end of auto-generated comment: release notes by coderabbit.ai -->

---------

Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

deepmd/pd/utils/dataloader.py
@@ -36,6 +36,10 @@ from deepmd.pd.utils import (
 from deepmd.pd.utils.dataset import (
     DeepmdDataSetForLoader,
 )
+from deepmd.pt.utils.utils import (
+    mix_entropy,
+)
+from deepmd.utils import random as dp_random
 from deepmd.utils.data import (
     DataRequirementItem,
 )
@@ -50,8 +54,13 @@ log = logging.getLogger(__name__)
 
 
 def setup_seed(seed):
-    paddle.seed(seed)
+    if isinstance(seed, (list, tuple)):
+        mixed_seed = mix_entropy(seed)
+    else:
+        mixed_seed = seed
+    paddle.seed(mixed_seed)
     os.environ["FLAGS_cudnn_deterministic"] = "True"
+    dp_random.seed(seed)
 
 
 class DpLoaderSet(Dataset):

deepmd/pt/utils/dataloader.py
@@ -36,6 +36,9 @@ from deepmd.pt.utils import (
 from deepmd.pt.utils.dataset import (
     DeepmdDataSetForLoader,
 )
+from deepmd.pt.utils.utils import (
+    mix_entropy,
+)
 from deepmd.utils.data import (
     DataRequirementItem,
 )
@@ -50,8 +53,12 @@ torch.multiprocessing.set_sharing_strategy("file_system")
 
 
 def setup_seed(seed) -> None:
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
+    if isinstance(seed, (list, tuple)):
+        mixed_seed = mix_entropy(seed)
+    else:
+        mixed_seed = seed
+    torch.manual_seed(mixed_seed)
+    torch.cuda.manual_seed_all(mixed_seed)
     torch.backends.cudnn.deterministic = True
     dp_random.seed(seed)
 

deepmd/pd/entrypoints/main.py
@@ -123,7 +123,7 @@ def get_trainer(
 
         # validation and training data
         # avoid the same batch sequence among devices
-        rank_seed = (seed + rank) % (2**32) if seed is not None else None
+        rank_seed = [rank, seed % (2**32)] if seed is not None else None
         validation_data_single = (
             DpLoaderSet(
                 validation_systems,

deepmd/pt/entrypoints/main.py
@@ -138,7 +138,7 @@ def get_trainer(
 
         # validation and training data
         # avoid the same batch sequence among devices
-        rank_seed = (seed + rank) % (2**32) if seed is not None else None
+        rank_seed = [rank, seed % (2**32)] if seed is not None else None
         validation_data_single = (
             DpLoaderSet(
                 validation_systems,

deepmd/tf/entrypoints/train.py
@@ -220,9 +220,10 @@ def _do_work(
     seed = jdata["training"].get("seed", None)
     if seed is not None:
         # avoid the same batch sequence among workers
-        seed += run_opt.my_rank
         seed = seed % (2**32)
-    dp_random.seed(seed)
+        dp_random.seed([run_opt.my_rank, seed])
+    else:
+        dp_random.seed(seed)
 
     # setup data modifier
     modifier = get_modifier(jdata["model"].get("modifier", None))

deepmd/utils/random.py
@@ -56,7 +56,7 @@ def random(size=None):
     return _RANDOM_GENERATOR.random_sample(size)
 
 
-def seed(val: Optional[int] = None) -> None:
+def seed(val: Optional[Union[int, list[int]]] = None) -> None:
     """Seed the generator.
 
     Parameters

commit 33d5ef5d37d04d7937928a890cdeedb0fb549fde

pd: fix typo in deepmd-kit-tmp/deepmd/pd/utils/dataloader.py (#4512)
Fix a typo introduced in #4479, which will cause an error if torch is
not installed.

![image](https://github.com/user-attachments/assets/3f9b0955-ec2b-46ac-9efa-1ed4b4e023eb)

cc @njzjz 

<!-- This is an auto-generated comment: release notes by coderabbit.ai
-->

## Summary by CodeRabbit

- **Chores**
- Updated the import path for the `mix_entropy` function to reflect a
reorganization of utility functions.

<!-- end of auto-generated comment: release notes by coderabbit.ai -->

Co-authored-by: Chun Cai <amoycaic@gmail.com>

deepmd/pd/utils/dataloader.py
@@ -36,7 +36,7 @@ from deepmd.pd.utils import (
 from deepmd.pd.utils.dataset import (
     DeepmdDataSetForLoader,
 )
-from deepmd.pt.utils.utils import (
+from deepmd.pd.utils.utils import (
     mix_entropy,
 )
 from deepmd.utils import random as dp_random